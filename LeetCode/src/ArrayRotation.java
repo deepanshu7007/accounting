3a4aeea88b9749e123b3ef369ac50bac5ca6717a
deepanshu7007 <deepanshu7007@gmail.com>
2022-01-26 20:01:07 +0530

    Rotation of an array using O(n) time

--- LeetCode/src/ArrayRotation.java
+++ LeetCode/src/ArrayRotation.java
@@ -1,33 +1,65 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+This is a game changer problem,because approch was unthinkable. 
+Approch behind this solution is reverse of an array from position arr.length-k
+then we have to reverse the first half from k and then second half
+and at last we have to reverse array arr using two pointers approch
+
+
+
+
+Problems:
+
+>This approch is far away from my mind's thought process
+>Cause reverse of an array in this solution is like unthinkable for me.
+
  */
 
 /**
  *
- * @author deepa
+ * @author lazyDrago
  */
 public class ArrayRotation {
 
     public static void main(String[] args) {
 
-        int nums[] = {1, 2, 3, 4, 5, 6, 7};
-        int k = 3;
-        int i=0;
-        int result1[] = new int[nums.length-k];
-        int result2[] = new int[k];
+        // int nums[] = {-1,-100,3,99};
+        int nums[] = {1,2};
+        int k = 2;
+        k=k%nums.length; // (*)
+        int l = nums.length-k, r = nums.length-1, temp = 0;
         
-        while(i<k)
-        {
-            result1[i]=nums[k+i];
-        i++;
-        }i=0;
-        while(i<nums.length-k)
-        {
-            result
-        i++;
+        while (l < r) {
+            temp = nums[r];
+            nums[r] = nums[l];
+            nums[l] = temp;
+            l++;
+            r--;
         }
-        
+        for(int i=0;i<nums.length;i++)
+        System.out.println(nums[i]);
+        System.out.println("\n\n");
+        l=0;
+        r = nums.length-1-k;
+        while (l < r) {
+            temp = nums[r];
+            nums[r] = nums[l];
+            nums[l] = temp;
+            l++;
+            r--;
+        }
+        for(int i=0;i<nums.length;i++)
+        System.out.println(nums[i]);
+        l=0;r=nums.length-1;
+        while(l<r)
+        {
+            temp = nums[r];
+            nums[r] = nums[l];
+            nums[l] = temp;
+            l++;
+            r--;
+        }
+        System.out.println("\n\n");
+        for(int i=0;i<nums.length;i++)
+        System.out.print(nums[i]+" ");
     }
 }
